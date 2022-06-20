b32f42b540090ea235a6f7fa6087e9d4c5bf4481
Sander Poks <sanderpoks@gmail.com>
2022-06-20 11:38:39 +0300

    Created basic project folder structure and added .gitignore

---
+++ src/shiftscheduler/main.py
@@ -0,0 +1,8 @@
+""" This program is meant for collecting employee requests for the next month's
+schedule and creating the schedule based on employee wants and department requirements """
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
